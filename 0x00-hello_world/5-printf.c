bb10f9cb9247ae4bd3969e4825d4a505bbc64565
asmaaamgad <asmaaatm22@example.com>
2023-08-12 13:36:09 -0700

    ${message}

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
+
 /**
- * main - Entry point 
- *
- * Description: A C program that prints with printf function.
+ * main - Entry point
  *
  * Return: Alwys 0 (success)
 */
-
 int main(void)
-}
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
