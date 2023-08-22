244ba3e7a2a5288238f352fbe26ab8b3dfe1913e
asmaaamgad <asmaaatm22@example.com>
2023-08-22 12:14:41 -0700

    hello

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,15 +2,14 @@
 #include <stdio.h>
 
 /**
- * rest_to_98 - check the code for Holberton School students.
+ * reset_to_98 - check the code for Holberton School students.
+ *
  * @n: int parameter
+ *
  * Return: nothing
  */
 
 void reset_to_98(int *n)
 {
 	*n = 98;
-
-	printf("%p\n", n);
-	printf("%d\n", n);
 }
