c4d624905b06d1d5861716d8e089819c0609eb31
asmaaamgad <asmaaatm22@example.com>
2023-08-22 12:11:29 -0700

    hello

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,15 +2,14 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- * pointer to an int as parameter and updates the value it points to to 98
- * Return: Always 0.
+ * rest_to_98 - check the code for Holberton School students.
+ * @n: int parameter
+ * Return: nothing
  */
 
 void reset_to_98(int *n)
 {
-	int *n = 98;
-	int s = 98;
+	*n = 98;
 
 	printf("%p\n", n);
 	printf("%d\n", n);
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,6 +3,16 @@
 
 #include <stdio.h>
 
+int _putchar(char C);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
 void reset_to_98(int *n);
 
 #endif /* MAIN_N */
