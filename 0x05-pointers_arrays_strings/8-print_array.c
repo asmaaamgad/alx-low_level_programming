555692a5705084ce1016aa210f4c4363aacdc120
asmaaamgad <asmaaatm22@example.com>
2023-08-22 16:14:51 -0700

    hello

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: nothing
+ */
+
+void print_array(int *a, int n)
+{
+	int array[6];
+
+	array = 0 + 1;
+	printf("%d, \n", array++);
+}
