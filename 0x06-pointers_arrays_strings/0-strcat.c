46a7694bea00eb3b6115a4fe942fa9253fb2714b
asmaaamgad <asmaaatm22@example.com>
2023-08-24 14:38:49 -0700

    hello

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,14 +11,14 @@
 
 char *_strcat(char *dest, char *src)
 {
-	char *_strcat(*dest, char *src)
-
-		int c, c2;
+	int main(void);
+	int c, c2;
 
 	c = 0;
 	while (dest[c])
 		c++;
-	for (c2 = 0; src[c2]; c2++)
-		dest[c++]; = src[2c];
+
+	for (c2 = 0; src[c2] ; c2++)
+		dest[c++] = src[c2];
 	return (dest);
 }
