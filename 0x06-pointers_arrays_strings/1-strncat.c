755ab6ddf96393ee9f2f230692cbc45f0a58676f
asmaaamgad <asmaaatm22@example.com>
2023-08-24 15:46:56 -0700

    hello

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,8 +1,8 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * *_strncat - function that concatenates two strings.
- * @desr: pointer to destination input
+ * @dest: pointer to destination input
  * @src: pointer to source input
  * @n: most numpers of bytes from @src
  *
@@ -13,19 +13,22 @@ char *_strncat(char *dest, char *src, int n)
 int main(void)
 	int c, i;
 
-	c = 0;
 	/*
 	 * find size of dest array*/
 	while (dest[c])
+	{
 		c++;
+	}
 	/*
 	 * src does not need to be pull terminated
 	 * if it contains n or more bytes
 	 */
-	for (i = 0; i < n; && src[i] != '\0'; i++)
-		dest [c + 1] = src[i];
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[c + 1] = src[i];
+	}
 	/*pull terminate dest*/
-	dest [c + i] = '\0';
+	dest[c + i] = '\0';
 	return (dest);
 
 }
